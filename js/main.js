ce3e03334887b6b679566ad5d1450aa66ff9b0ef
Jeanne D'Arc <jeanne.ngassa@gmail.com>
2016-11-11 20:52:29 +0100

    réglage de la géolocalisation

--- js/main.js
+++ js/main.js
@@ -144,7 +144,7 @@ jQuery(function($) {
 			scrollwheel: false,
 			center: myLatlng
 		};
-		var map = new google.maps.Map(document.getElementById('#google-map'), mapOptions);
+		var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
 		var contentString = '';
 		var infowindow = new google.maps.InfoWindow({
 			content: '<div class="map-content"><ul class="address">' + $('.address').html() + '</ul></div>'
